5ad83fa46ff45e16bf3e1b1e3ee6666ce7b3fca7
Arnav Dixit <arnav.dixit@essencetechlabs.com>
2026-04-15 13:15:26 +0530

    made changes in Public

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import portfolioLogo from "./assets/logo/temp.png";
 
 const existingFavicon = document.querySelector("link[rel='icon']");
 const favicon = existingFavicon || document.createElement("link");
 
 favicon.setAttribute("rel", "icon");
 favicon.setAttribute("type", "image/png");
-favicon.setAttribute("href", portfolioLogo);
+favicon.setAttribute("href", "/favicon.png");
 
 if (!existingFavicon) {
   document.head.appendChild(favicon);
